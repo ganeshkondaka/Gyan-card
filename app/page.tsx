e29c7156fef1f5c228c51ad4527fecb5b720c30d
ganeshkondaka <kondakaganesh2003@outlook.com>
2024-12-01 12:01:05 +0530

    added a small style

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
           </div>
 
           {/* Features Grid */}
-          <div className="pt-5 grid grid-cols-1 md:grid-cols-3 gap-8 mt-20">
+          <div className=" pb-5 pt-5 grid grid-cols-1 md:grid-cols-3 gap-8 mt-20">
             {[
               {
                 title: 'Professional Design',
@@ -86,12 +86,12 @@ export default function Home() {
           {/* Bottom Badge */}
           <div className="mt-16 inline-flex items-center justify-center gap-2 bg-gray-700 px-6 py-3 rounded-full">
             <span className="text-sm font-medium text-gray-300">
-              <Link href={'/pages/signup'} className="text-yellow-400 hover:underline">
+              <Link href={'/pages/signup'} className="inline-flex items-center gap-2 bg-zinc-200 text-gray-900 px-8 py-4 rounded-full font-medium hover:bg-yellow-500 transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl">
                 Click here to Get started
               </Link>
             </span>
           </div>
-          <span className="text-xl pt-5 font-bold block text-yellow-400 animate-bounce">
+          <span className="text-xl pt-5 font-bold block text-green-600 animate-bounce">
             Under Development...
           </span>
           {/* <Upload_images></Upload_images> */}
